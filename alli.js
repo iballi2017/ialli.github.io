fdb927a577cf6c9cd19d7c96a6c1f365bf786e1a
Alli Ibrahim <iballi2017@gmail.com>
2018-09-16 20:54:09 -0700

    update

--- alli.js
+++ alli.js
@@ -1,9 +1,16 @@
 x=0;
-function openOmenu(){
+function openMenu(){
     if(x==0){
     document.getElementById("mobileMenu").style.display="block"; x=1;
+    document.getElementById("menu-icon").style.display="none"; x=1;
+    document.getElementById("close-icon").style.display="block"; x=1;
     }
-    else if(x==1){
-    document.getElementById("mobileMenu").style.display="none"; x=0;
-    }
+ }
+
+ function closeMenu(){
+    if(x==1){
+        document.getElementById("mobileMenu").style.display="none"; x=0;
+        document.getElementById("menu-icon").style.display="block"; x=0;
+        document.getElementById("close-icon").style.display="none"; x=0;
+        }
  }
